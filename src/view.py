04d1b21324bcfc0852403fd7e772e3c0d21d3fc4
tsubo5510 <ssnk4138tsu@gmail.com>
2023-09-06 00:14:17 +0900

    1st commmit

---
+++ src/view.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+#　必要なモジュールのインポート
+from flask import Flask,render_template
+
+# app という変数でFlaskオブジェクトをインスタンス化
+app = Flask(__name__)
+
+# --- View側の設定 ---
+# ルートディレクトリにアクセスした場合の挙動
+@app.route('/')
+# def以下がアクセス後の操作
+def index():
+    # return 'Hello World!'
+    return render_template('index.html')
+
+# エントリーポイント
+if __name__ == '__main__':
+    app.run()
+
